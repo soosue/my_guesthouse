c3b5945d0644460841d59aa2f6c9a78d66b2848f
soo <yunsoo0510@daum.net>
2023-03-04 19:57:34 +0900

    feat: 리뷰 API에 OpenAPI 적용

--- src/main/java/com/java/guesthouse/review/service/dto/UpdateReviewRequest.java
+++ src/main/java/com/java/guesthouse/review/service/dto/UpdateReviewRequest.java
@@ -1,7 +1,12 @@
 package com.java.guesthouse.review.service.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "리뷰")
 public record UpdateReviewRequest(
+        @Schema(description = "별점", defaultValue = "5", allowableValues = {"1", "2", "3", "4", "5"})
         Integer rate,
+        @Schema(description = "리뷰내용", defaultValue = "")
         String content
 ) {
 }
