45210dd19f594fa73c46a5c9748ea8f3b43d26fb
soo <yunsoo0510@daum.net>
2023-03-04 19:31:22 +0900

    refactor: springdoc 적용을 위한 url 변경

--- src/main/java/com/java/guesthouse/point/ui/PointController.java
+++ src/main/java/com/java/guesthouse/point/ui/PointController.java
@@ -6,15 +6,17 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.java.guesthouse.point.service.PointService;
 import com.java.guesthouse.point.service.dto.PointAccumulatesResponse;
 import com.java.guesthouse.point.service.dto.PointResponse;
 import com.java.guesthouse.point.service.dto.PointUsesResponse;
 
-@Controller
+@RestController
+@RequestMapping("/v1/points")
 public class PointController {
     private final PointService pointService;
 
@@ -22,13 +24,13 @@ public class PointController {
         this.pointService = pointService;
     }
 
-    @GetMapping("/v1/points/me")
+    @GetMapping("/me")
     public ResponseEntity<PointResponse> getMyPoint(HttpSession session) {
         Long memberId = getMemberId(session);
         return ResponseEntity.ok(pointService.getPointByMemberId(memberId));
     }
 
-    @GetMapping("/v1/point-accumulates/me")
+    @GetMapping("/accumulates/me")
     public ResponseEntity<PointAccumulatesResponse> getMyPointAccumulates(
             HttpSession session,
             @PageableDefault(size = 5, sort = "id", direction = Sort.Direction.DESC) Pageable pageable
@@ -37,7 +39,7 @@ public class PointController {
         return ResponseEntity.ok(pointService.getPointAccumulates(memberId, pageable));
     }
 
-    @GetMapping("/v1/point-uses/me")
+    @GetMapping("/uses/me")
     public ResponseEntity<PointUsesResponse> getMyPointUses(
             HttpSession session,
             @PageableDefault(size = 5, sort = "id", direction = Sort.Direction.DESC) Pageable pageable
